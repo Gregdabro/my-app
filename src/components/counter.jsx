0a723248cf0a124f5e517ef3b15406e3093398a9
Greg <gregmonak@gmail.com>
2022-07-04 15:48:09 +0200

    11. Условный рендеринг

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -33,21 +33,27 @@ const Counter =()=> {
         // setTags(["tag4", "tag5"])
         setTags(prevState => prevState.filter(tag => tag !== id));
         console.log(id)
+    }
 
+    const renderTags = () => {
+        return (tags.length !== 0 && tags.map((tag) => (
+                <li key={tag}
+                    className="btn btn-primary btn-sm m-2"
+                    onClick={() => handleTagChange(tag)}>
+                    {tag}
+                </li>
+            ))
+        );
+    };
+
+    // условие для рендеринга
+    if (tags.length !==0) {
+        return <ul>{renderTags()}</ul>
     }
 
     return (
         // <React.Fragment></React.Fragment> можно сократить оставив тег пустым
         <>
-            <ul>
-                {tags.map((tag) => (
-                    <li key={tag}
-                        className="btn btn-primary btn-sm m-2"
-                        onClick={() => handleTagChange(tag)}>
-                        {tag}
-                    </li>
-                ))}
-            </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button
                 className="btn btn-primary btn-sm m-2"
