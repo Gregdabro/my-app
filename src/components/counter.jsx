552e9b3849b656ef6b9122e39d4b967c32574b63
Greg <gregmonak@gmail.com>
2022-07-04 11:49:09 +0200

    4. Вложенные выражения

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 // создаем компонент
 const Counter =()=> {
-    return <h1>Counter</h1>;
+    const count = 0;
+    // можем использовать переменные
+    // const x = <h1>empty</h1>;
+
+    const formatCount = () => {
+        return count === 0 ? "empty" : count;
+    }
+
+    return (
+        // <React.Fragment></React.Fragment> можно сократить оставив тег пустым
+        <>
+            <span>{formatCount()}</span>
+            <button>+</button>
+        </>
+    );
 };
 // экспортируем компонент
 export default Counter;
