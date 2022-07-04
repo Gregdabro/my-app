14eca9ae1df41d3474fbc04f45c7bebe8292feef
Greg <gregmonak@gmail.com>
2022-07-04 12:32:58 +0200

    7. Обработка событий

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // создаем компонент
 const Counter =()=> {
-    const count = 0;
+    let count = 0;
     // можем использовать переменные
     // const x = <h1>empty</h1>;
 
@@ -17,11 +17,17 @@ const Counter =()=> {
         return classes;
     }
 
+    //обраюотчики событий
+    const handleIncrement = () => {
+        count = count +1;
+        console.log('count', count)
+    }
+
     return (
         // <React.Fragment></React.Fragment> можно сократить оставив тег пустым
         <>
             <span className={getBadgeClasses()}>{formatCount()}</span>
-            <button className="btn btn-primary btn-sm m-2">+</button>
+            <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
         </>
     );
 };
