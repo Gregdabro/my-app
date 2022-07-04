f94caf4cf17ee8113b116470c67f610fcd04d2fe
Greg <gregmonak@gmail.com>
2022-07-04 14:09:30 +0200

    9. Рендер списка из массива

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 const Counter =()=> {
     // содержание useState может быть любим(numbers,object,array...any)
     const [count, setCount] = useState(0);
+    const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
     // можем использовать переменные
     // const x = <h1>empty</h1>;
@@ -28,9 +29,22 @@ const Counter =()=> {
         setCount((prevState) => prevState - 1);
     }
 
+    const handleTagChange = () => {
+        setTags(["tag4", "tag5"])
+    }
+
     return (
         // <React.Fragment></React.Fragment> можно сократить оставив тег пустым
         <>
+            <ul>
+                {tags.map((tag) => (
+                    <li key={tag}
+                        className="btn btn-primary btn-sm m-2"
+                        onClick={handleTagChange}>
+                        {tag}
+                    </li>
+                ))}
+            </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button
                 className="btn btn-primary btn-sm m-2"
