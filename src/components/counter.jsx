7cb7a12e87da3d305def49772c9a6580f7e0bc7a
Greg <gregmonak@gmail.com>
2022-07-04 13:02:25 +0200

    useState()

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 // создаем компонент
 const Counter =()=> {
-    let count = 0;
+    // содержание useState может быть любим(numbers,object,array...any)
+    const [count, setCount] = useState(0);
+
     // можем использовать переменные
     // const x = <h1>empty</h1>;
 
@@ -19,15 +21,27 @@ const Counter =()=> {
 
     //обраюотчики событий
     const handleIncrement = () => {
-        count = count +1;
-        console.log('count', count)
+        setCount((prevState) => prevState + 1);
+    }
+
+    const handleDecrement = () => {
+        setCount((prevState) => prevState - 1);
     }
 
     return (
         // <React.Fragment></React.Fragment> можно сократить оставив тег пустым
         <>
             <span className={getBadgeClasses()}>{formatCount()}</span>
-            <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
+            <button
+                className="btn btn-primary btn-sm m-2"
+                onClick={handleIncrement}>
+                +
+            </button>
+            <button
+                className="btn btn-primary btn-sm m-2"
+                onClick={handleDecrement}>
+                -
+            </button>
         </>
     );
 };
